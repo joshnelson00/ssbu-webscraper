aca7f62b96a15fb99251ab41e36e5249cfaa6052
Josh Nelson <joshuaknelson1202@icloud.com>
2024-10-10 19:38:33 -0700

    Added LICENSE

--- scraper.py
+++ scraper.py
@@ -1,6 +1,7 @@
 import time
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup
 from selenium.webdriver.support.ui import WebDriverWait
@@ -9,6 +10,9 @@ from selenium.webdriver.support import expected_conditions as EC
 options = webdriver.ChromeOptions() 
 options.add_argument("--start-maximized") 
 options.add_argument('--log-level=3') 
+chrome_options.add_argument("--headless")  # Run Chrome in headless mode
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--disable-dev-shm-usage")
 
 service = Service(executable_path = "./chromedriver", chrome_options=options)
 driver = webdriver.Chrome(service = service)
