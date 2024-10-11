152a7789af4b7de1cabaeadd6c13de87ec8525eb
Josh Nelson <joshuaknelson1202@icloud.com>
2024-10-10 20:03:32 -0700

    Added LICENSE

--- scraper.py
+++ scraper.py
@@ -1,7 +1,7 @@
 import time
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import options
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup
 from selenium.webdriver.support.ui import WebDriverWait
@@ -14,7 +14,7 @@ options.add_argument('--disable-dev-shm-usage')
 options.add_argument('--disable-gpu')
 
 service = Service(executable_path = "./chromedriver", chrome_options=options)
-driver = webdriver.Chrome(service = service)
+driver = webdriver.Chrome(service = service, options=options)
 
 driver.get("https://www.start.gg/tournament/firefox-friday-120/attendees")
 
